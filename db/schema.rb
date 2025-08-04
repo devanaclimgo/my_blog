bde0fdb7d15bc1b6120ade57cf4dcfe1a31a4cd2
devanaclimgo <anaclimgo@gmail.com>
2025-08-04 18:32:57 -0300

    feat: create posts migrated to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_07_202132) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_04_212819) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
